356f4e83720935461c4060b64d1f3e5d453156a9
marthaberner <marthaberner.mb@gmail.com>
2015-08-27 08:05:22 -0600

    initial commit

---
+++ db/seeds.js
@@ -0,0 +1,38 @@
+const db = require('monk')('localhost/posts-workshop')
+const Post = db.get('posts')
+
+Post.remove({})
+
+.then(function() {
+
+  return Promise.all([
+    Post.insert({
+      title: "My Big Adventure",
+      author: "Jeff Dean",
+      image: "http://www.planwallpaper.com/static/images/image5_170127819.jpg",
+      description: "guy fising in tux",
+      create_at: new Date
+    }),
+
+    Post.insert({
+      title: "My Other Adventure",
+      author: "Jeff Dean",
+      image: "http://www.planwallpaper.com/static/images/image5_170127819.jpg",
+      description: "guy fising in tux",
+      create_at: new Date
+    }),
+
+    Post.insert({
+      title: "My Third Adventure",
+      author: "Jeff Dean",
+      image: "http://www.planwallpaper.com/static/images/image5_170127819.jpg",
+      description: "guy fising in tux",
+      create_at: new Date
+    }),
+  ])
+
+})
+
+.then(function(){
+  db.close();
+})
